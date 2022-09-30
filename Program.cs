0cc1758a6cd53ff34a1ab210e07952c20b9ba503
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2022-09-30 10:51:31 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Compression;
+using System.Xml;
+
+var path = "exemplo.docx";
+
+using var archive = ZipFile.Open(path, ZipArchiveMode.Update);
+var document = archive.GetEntry("word/document.xml")!;
+
+using var documentStream = document.Open();
+var xml = new XmlDocument();
+xml.Load(documentStream);
+
+var changed = xml.OuterXml.Replace("Oi", "Olá");
+xml.LoadXml(changed);
+
+documentStream.Seek(0, SeekOrigin.Begin);
+xml.Save(documentStream);
